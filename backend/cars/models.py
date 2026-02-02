94364b2daabc1b0781a5b65e208e0952d1fdc521
KlajdiMexhaj <mexhajklajdi@gmail.com>
2026-02-02 23:01:09 +0100

    Car detail changes commit

--- backend/cars/models.py
+++ backend/cars/models.py
@@ -4,15 +4,24 @@ from django.contrib.postgres.fields import JSONField
 # Create your models here.
 
 class Car(models.Model):
-    name = models.CharField(max_length=200,blank=True,null=True)
-    image = models.ImageField(upload_to='cars/',blank=True,null=True)
-    detail = models.TextField(blank=True,null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2,blank=True,null=True)
+    TRANSMISSION_CHOICES = [
+        ('manual', 'Manual'),
+        ('automatic', 'Automatic'),
+    ]
 
-    def __str__(self):
-        return self.name
-    
+    name = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='cars/', blank=True, null=True)
+    detail = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
+    seats = models.PositiveSmallIntegerField(blank=True, null=True)
+    transmission = models.CharField(
+        max_length=10,
+        blank=True, null=True,
+        choices=TRANSMISSION_CHOICES
+    )
+    air_conditioning = models.BooleanField(default=False, blank=True, null=True)
+    doors = models.PositiveSmallIntegerField(blank=True, null=True)
 
 
 class Reservation(models.Model):
