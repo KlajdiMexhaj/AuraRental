d075ca13cf7b32bb1bcca1d821b478eee86646ad
KlajdiMexhaj <mexhajklajdi@gmail.com>
2026-02-19 19:52:45 +0100

    Last commit reviews 1

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,7 +8,7 @@ import Footer from '../components/Footer';
 import { fetchCars } from '../services/api';
 import type { Car } from '../types';
 import { Link,useLocation } from 'react-router-dom';
-import Reviews from '../components/REviews';
+import Review from '../components/Review';
 const Home: React.FC = () => {
   const location = useLocation();
 
@@ -73,7 +73,7 @@ useEffect(() => {
       </section>
 
       <About />
-      <Reviews />
+      <Review />
       <Location />
       <Footer />
     </div>
